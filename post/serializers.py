50e3372be5e207740a22428e07c4cf56e85d5ba5
BAYA <bayaman2101@gmail.com>
2022-10-30 21:15:25 +0600

    delet seria

--- post/serializers.py
+++ post/serializers.py
@@ -1,26 +1,2 @@
-from django.db.models import Avg
-from rest_framework import serializers
-from .models import Post, Comment, Rating
 
 
-class PostSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Post
-        fields = ['author', 'text', 'created']
-
-    avg_rating = serializers.SerializerMethodField()
-
-    def get_avg_rating(self, ob):
-        return ob.reviews.all().aggregate(Avg('rating'))['rating__avg']
-
-
-class RatingSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Rating
-        fields = ['score', 'rating']
-
-
-class CommentSerializers(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
-        fields = '__all__'
