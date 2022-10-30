356f1f1e6f440a33f83b6578ec5f5344873b58de
BAYA <bayaman2101@gmail.com>
2022-10-30 21:30:54 +0600

    urls changed

--- post/urls.py
+++ post/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, include
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
-    path('register/', views.RegisterView.as_view()),
-    path('', include('rest_framework.urls')),
-    path('token/', obtain_auth_token),
-]
+    path('post/', views.PostListCreateView.as_view()),
+    path('post/<int:pk>/', views.PostRetrieveUpdateDeleteView.as_view()),
+    path('post/<int:post_id>/comment/', views.CommentListCreateAPIView.as_view()),
+    path('post/<int:post_id>/comment/<int:pk>/', views.CommentRetrieveDestroyUpdateAPIView.as_view()),
+    path('post/mark', views.MarkListCreateView.as_view()),
+    path('post/mark/<int:pk>/', views.MarkRetrieveDestroyUpdateAPIView.as_view()),
+    ]
