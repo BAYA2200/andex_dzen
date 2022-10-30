2943aeceaf93a2be924803ee374e693fd759dfd8
BAYA <bayaman2101@gmail.com>
2022-10-30 21:06:33 +0600

    Add model

--- account/models.py
+++ account/models.py
@@ -1,11 +1,10 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
-User = get_user_model()
+class User(AbstractUser):
+    telegram = models.IntegerField(null=True)
 
-
-class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='author')
-    telegram_chat_id = models.IntegerField()
-
+    def __str__(self):
+        return self.username
